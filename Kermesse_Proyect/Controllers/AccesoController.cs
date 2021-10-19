f2d7e1f16d4b42ecd5f906303ed3e12560152c06
Cristian de Jesús Zamora Rizo <wcrisamzo12@gmail.com>
2021-10-19 07:26:17 -0600

    Proyect Kermesse

---
+++ Kermesse_Proyect/Controllers/AccesoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kermesse_Proyect.Models;
+
+namespace Kermesse_Proyect.Controllers
+{
+    public class AccesoController : Controller
+    {
+
+        private BDKermesseEntities db = new BDKermesseEntities();
+
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost] 
+        public ActionResult Login(string email, string pwd)
+        {
+            try
+            {
+                var oUser = (from u in db.Usuario where u.email == email.Trim() && u.pwd == pwd.Trim() select u).FirstOrDefault();
+                if(oUser == null)
+                {
+                    ViewBag.Error = "Usuario o Contraseña Invalida";
+                    return View();
+                }
+
+                Session["User"] = oUser;
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch(Exception e)
+            {
+                ViewBag.Error = e.Message;
+                return View();
+            }
+        }
+    }
+}
---
+++ Kermesse_Proyect/Filters/VerificarSession.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kermesse_Proyect.Models;
+using Kermesse_Proyect.Controllers;
+
+namespace Kermesse_Proyect.Filters
+{
+    public class VerificarSession : ActionFilterAttribute
+    {
+        private Usuario oUsuario;
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+                base.OnActionExecuting(filterContext);
+
+                oUsuario = (Usuario)HttpContext.Current.Session["User"];
+                if (oUsuario == null)
+                {
+                    if (filterContext.Controller is AccesoController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("/Acceso/Login");
+                    }
+                }
+            }
+            catch(Exception)
+            {
+                filterContext.Result = new RedirectResult("~/Acceso/Login");
+            }
+        }
+    }
+}
